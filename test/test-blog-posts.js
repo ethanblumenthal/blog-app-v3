b9f9c89da24b9a0ed5ee0093d4cf07e08269d41a
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-24 15:37:11 -0500

    GET endpoint

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -55,4 +55,48 @@ describe('blog posts API resource', function () {
   after(function () {
     return closeServer();
   });
+
+  describe('GET endpoint', function () {
+    it('should return all existing posts', function () {
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(_res => {
+          res = _res;
+          res.should.have.status(200);
+          res.body.should.have.lengthOf.at.least(1);
+
+          return BlogPost.count();
+        })
+        .then(count => {
+          res.body.should.have.lengthOf(count);
+        });
+    });
+
+    it('should return posts with right fields', function () {
+      let resPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(function (res) {
+
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.lengthOf.at.least(1);
+
+          res.body.forEach(function (post) {
+            post.should.be.a('object');
+            post.should.include.keys('id', 'title', 'content', 'author', 'created');
+          });
+          resPost = res.body[0];
+          return BlogPost.findById(resPost.id);
+        })
+        .then(post => {
+          resPost.title.should.equal(post.title);
+          resPost.content.should.equal(post.content);
+          resPost.author.should.equal(post.authorName);
+        });
+    });
+  });
+
 });
