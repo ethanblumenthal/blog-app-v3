d2e32ea3b65de11309895569381fcbadbdffa51b
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-24 15:38:12 -0500

    POST endpoint

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -99,4 +99,39 @@ describe('blog posts API resource', function () {
     });
   });
 
+  describe('POST endpoint', function () {
+    it('should add a new blog post', function () {
+      const newPost = {
+        title: faker.lorem.sentence(),
+        author: {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+        },
+        content: faker.lorem.text()
+      };
+      return chai.request(app)
+        .post('/posts')
+        .send(newPost)
+        .then(function (res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys(
+            'id', 'title', 'content', 'author', 'created');
+          res.body.title.should.equal(newPost.title);
+          // cause Mongo should have created id on insertion
+          res.body.id.should.not.be.null;
+          res.body.author.should.equal(
+            `${newPost.author.firstName} ${newPost.author.lastName}`);
+          res.body.content.should.equal(newPost.content);
+          return BlogPost.findById(res.body.id);
+        })
+        .then(function (post) {
+          post.title.should.equal(newPost.title);
+          post.content.should.equal(newPost.content);
+          post.author.firstName.should.equal(newPost.author.firstName);
+          post.author.lastName.should.equal(newPost.author.lastName);
+        });
+    });
+  });
 });
